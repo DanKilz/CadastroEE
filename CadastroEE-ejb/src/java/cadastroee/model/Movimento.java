e59e92e66a9ee76c8adb234a28d237ae84cafbcd
Daniel Kilzer <danielkilzer@yahoo.com.br>
2025-02-12 21:47:14 -0300

    Commit inicial - Procedimento 1

---
+++ CadastroEE-ejb/src/java/cadastroee/model/Movimento.java
@@ -0,0 +1,182 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author danie
+ */
+@Entity
+@Table(name = "Movimento")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Movimento.findAll", query = "SELECT m FROM Movimento m"),
+    @NamedQuery(name = "Movimento.findByIdMovimento", query = "SELECT m FROM Movimento m WHERE m.idMovimento = :idMovimento"),
+    @NamedQuery(name = "Movimento.findByTipoMovimento", query = "SELECT m FROM Movimento m WHERE m.tipoMovimento = :tipoMovimento"),
+    @NamedQuery(name = "Movimento.findByQuantidade", query = "SELECT m FROM Movimento m WHERE m.quantidade = :quantidade"),
+    @NamedQuery(name = "Movimento.findByValorUnitario", query = "SELECT m FROM Movimento m WHERE m.valorUnitario = :valorUnitario"),
+    @NamedQuery(name = "Movimento.findByDataMovimento", query = "SELECT m FROM Movimento m WHERE m.dataMovimento = :dataMovimento")})
+public class Movimento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idMovimento")
+    private Integer idMovimento;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 6)
+    @Column(name = "tipoMovimento")
+    private String tipoMovimento;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "quantidade")
+    private int quantidade;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "valorUnitario")
+    private BigDecimal valorUnitario;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "dataMovimento")
+    @Temporal(TemporalType.DATE)
+    private Date dataMovimento;
+    @JoinColumn(name = "idPessoa", referencedColumnName = "idPessoa")
+    @ManyToOne(optional = false)
+    private Pessoa idPessoa;
+    @JoinColumn(name = "idProduto", referencedColumnName = "idProduto")
+    @ManyToOne(optional = false)
+    private Produto idProduto;
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario")
+    @ManyToOne(optional = false)
+    private Usuario idUsuario;
+
+    public Movimento() {
+    }
+
+    public Movimento(Integer idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public Movimento(Integer idMovimento, String tipoMovimento, int quantidade, BigDecimal valorUnitario, Date dataMovimento) {
+        this.idMovimento = idMovimento;
+        this.tipoMovimento = tipoMovimento;
+        this.quantidade = quantidade;
+        this.valorUnitario = valorUnitario;
+        this.dataMovimento = dataMovimento;
+    }
+
+    public Integer getIdMovimento() {
+        return idMovimento;
+    }
+
+    public void setIdMovimento(Integer idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public String getTipoMovimento() {
+        return tipoMovimento;
+    }
+
+    public void setTipoMovimento(String tipoMovimento) {
+        this.tipoMovimento = tipoMovimento;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Date getDataMovimento() {
+        return dataMovimento;
+    }
+
+    public void setDataMovimento(Date dataMovimento) {
+        this.dataMovimento = dataMovimento;
+    }
+
+    public Pessoa getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Pessoa idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public Produto getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Produto idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idMovimento != null ? idMovimento.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Movimento)) {
+            return false;
+        }
+        Movimento other = (Movimento) object;
+        if ((this.idMovimento == null && other.idMovimento != null) || (this.idMovimento != null && !this.idMovimento.equals(other.idMovimento))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cadastroee.model.Movimento[ idMovimento=" + idMovimento + " ]";
+    }
+    
+}
